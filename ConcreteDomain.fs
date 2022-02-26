244f453f4fb75dd495e5eef31feea9c126222448
makaj <makaj@LAPTOP-6SMO4K99>
2022-02-26 18:05:21 +0100

    Aggiungere i file di progetto.

---
+++ ConcreteDomain.fs
@@ -0,0 +1,16 @@
+﻿module ConcreteDomain
+
+//Expression
+type expr = 
+    | Int of int 
+    | Var of string
+    | BinOp of expr * string * expr 
+
+//Statement
+type stm = 
+    | Assign of string * expr
+    | IfThenElse of expr * statement * statement option
+    | While of expr * statement
+
+//Program
+type prog = Prog of statement list
